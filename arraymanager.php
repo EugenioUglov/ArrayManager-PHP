db1bb9190461738c9a844fc0f0da8ba24858ee58
Eugenio <68464938+EugenioUglov@users.noreply.github.com>
2022-11-18 16:56:12 +0100

    Update arraymanager.php

--- arraymanager.php
+++ arraymanager.php
@@ -15,4 +15,41 @@ class ArrayManager {
 
         return false;
     }
+
+    function stringify($input_array, $additional_separator = "<br>", $position_mode = "vertical") {
+        $output_str = "";
+
+        foreach($input_array as $item) {
+            strlen($output_str) > 0 && strlen($item) > 0 ? $output_str .= $additional_separator : ""; 
+            $output_str .= $item;
+        }
+
+        return $output_str;
+    }
+
+    function to_string(Object $option) {
+        $default_option = (object)array(
+            "input_array" => null,
+            "separator" => ", <br>",
+            "position_mode"  => "vertical"
+        );
+
+        if (isset($option->input_array) === false || is_array($option->input_array) === false) {
+            echo "<script>alert('Error! In function to_string() from Array_manages.cs  input_array is not defined');</script>";
+            return "";
+        }
+
+        $input_array = $option->input_array;
+        $separator = isset($option->separator) ? $option->separator : $default_option->separator;
+
+        $output_str = "";
+
+
+        foreach($input_array as $item) {
+            strlen($output_str) > 0 && strlen($item) > 0 ? $output_str .= $separator : ""; 
+            $output_str .= $item;
+        }
+
+        return $output_str;
+    }
 }
